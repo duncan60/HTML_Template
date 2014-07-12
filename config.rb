5d1464493e7aa0db97e746bf634d59a6c3492bd7
duncan_du <tmduncan60@gmail.com>
2014-07-12 23:49:36 +0800

    update

--- config.rb
+++ config.rb
@@ -1,12 +1,12 @@
 # Require any additional compass plugins here.
-
 # Set this to the root of your project when deployed:
-http_path = "/"
+http_path = "../"
 css_dir = "css"
 sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
-line_comments = false
+#line_comments = false
+#output_style=:compressed
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
--- js/ga_public.js
+++ js/ga_public.js
@@ -1,6 +1,6 @@
-﻿//=====================================================GA
+﻿// google Analytics A==============
  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', '---ga id--']);
+  _gaq.push(['_setAccount', 'ga id']);
  _gaq.push(['_trackPageview']);
 
   (function() {
@@ -11,10 +11,30 @@
 
 
 function addPageView(str){
-	_gaq.push(['_trackPageview',str]);
-	//console.log("str:"+str);
+  _gaq.push(['_trackPageview',str]);
+  //console.log("str:"+str);
 }
-function addEvent(evt,str){
-	_gaq.push(['_trackEvent',evt,str]);
-	//console.log("evt:"+evt+"=====str:"+str);
+function addEvent(id,evt,str){
+  _gaq.push(['_trackEvent',id,evt,str]);
+  //console.log("evt:"+evt+"=====str:"+str);
+}
+
+
+
+// google Analytics B==============
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'ga id', 'domain');
+
+// google Analytics END===========
+
+function trackpage(someurl){
+  ga('send', 'pageview', someurl);
+}
+
+function trackevent(tk){
+  ga('send', 'event', 'button', 'click', tk);
 }
